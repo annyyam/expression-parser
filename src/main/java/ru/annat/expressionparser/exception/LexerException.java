138d2772e76358565674fdb3228717391f70871d
annyy <annyyam@yandex.ru>
2026-04-20 16:32:10 +0300

    добавлен класс LexerException

---
+++ src/main/java/ru/annat/expressionparser/exception/LexerException.java
@@ -0,0 +1,8 @@
+package ru.annat.expressionparser.exception;
+
+public class LexerException extends RuntimeException {
+
+    public LexerException(String message) {
+        super(message);
+    }
+}
