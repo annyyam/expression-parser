6a87007ad26d6a44cccf8b9186a59cd9920ac557
annyy <annyyam@yandex.ru>
2026-04-20 16:14:15 +0300

    добавлен класс TokenType

---
+++ src/main/java/ru/annat/expressionparser/lexer/TokenType.java
@@ -0,0 +1,14 @@
+package ru.annat.expressionparser.lexer;
+
+public enum TokenType {
+    NUMBER,
+    IDENTIFIER,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    COMMA,
+    EOF
+}
