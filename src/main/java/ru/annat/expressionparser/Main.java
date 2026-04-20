bbd5ab0088aa40f875a78ff1f5fd7896bbdfae26
annyy <annyyam@yandex.ru>
2026-04-20 21:45:42 +0300

    реализован Parser: expression, term, unary, primary parsing + проверен в Main

--- src/main/java/ru/annat/expressionparser/Main.java
+++ src/main/java/ru/annat/expressionparser/Main.java
@@ -1,20 +1,28 @@
 package ru.annat.expressionparser;
 
+import ru.annat.expressionparser.ast.Expression;
 import ru.annat.expressionparser.lexer.Lexer;
 import ru.annat.expressionparser.lexer.Token;
+import ru.annat.expressionparser.parser.Parser;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        String expression = "sqrt(a) + 2 * b";
+        String expressionText = "(2 + 3) * 4";
 
-        Lexer lexer = new Lexer(expression);
+        Lexer lexer = new Lexer(expressionText);
         List<Token> tokens = lexer.tokenize();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expression expression = parser.parse();
+
+        Map<String, Double> variables = new HashMap<>();
+        double result = expression.evaluate(variables);
+
+        System.out.println("Result: " + result);
     }
 }
---
+++ src/main/java/ru/annat/expressionparser/parser/Parser.java
@@ -0,0 +1,122 @@
+package ru.annat.expressionparser.parser;
+
+import ru.annat.expressionparser.ast.Expression;
+import ru.annat.expressionparser.exception.ParseException;
+import ru.annat.expressionparser.lexer.Token;
+import ru.annat.expressionparser.lexer.TokenType;
+import ru.annat.expressionparser.ast.BinaryExpression;
+import ru.annat.expressionparser.ast.UnaryExpression;
+import ru.annat.expressionparser.ast.NumberExpression;
+import ru.annat.expressionparser.ast.VariableExpression;
+
+import java.util.List;
+
+public class Parser {
+
+    private final List<Token> tokens;
+    private int position;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+        this.position = 0;
+    }
+
+    public Expression parse() {
+        Expression expression = parseExpression();
+
+        if (!isAtEnd()) {
+            throw new ParseException("Unexpected token at position " + current().getPosition());
+        }
+
+        return expression;
+    }
+
+    private Expression parseExpression() {
+        Expression expression = parseTerm();
+
+        while (match(TokenType.PLUS, TokenType.MINUS)) {
+            Token operator = previous();
+            Expression right = parseTerm();
+            expression = new BinaryExpression(expression, operator.getText().charAt(0), right);
+        }
+
+        return expression;
+    }
+
+    private Expression parseTerm() {
+        Expression expression = parseUnary();
+
+        while (match(TokenType.MULTIPLY, TokenType.DIVIDE)) {
+            Token operator = previous();
+            Expression right = parseUnary();
+            expression = new BinaryExpression(expression, operator.getText().charAt(0), right);
+        }
+
+        return expression;
+    }
+    private Expression parseUnary() {
+        if (match(TokenType.PLUS, TokenType.MINUS)) {
+            Token operator = previous();
+            Expression right = parseUnary();
+            return new UnaryExpression(operator.getText().charAt(0), right);
+        }
+
+        return parsePrimary();
+    }
+    private Expression parsePrimary() {
+        if (match(TokenType.NUMBER)) {
+            Token number = previous();
+            return new NumberExpression(Double.parseDouble(number.getText()));
+        }
+
+        if (match(TokenType.IDENTIFIER)) {
+            Token identifier = previous();
+            return new VariableExpression(identifier.getText());
+        }
+
+        if (match(TokenType.LEFT_PAREN)) {
+            Expression expression = parseExpression();
+
+            if (!match(TokenType.RIGHT_PAREN)) {
+                throw new ParseException("Expected ')' at position " + current().getPosition());
+            }
+
+            return expression;
+        }
+
+        throw new ParseException("Expected number, variable or '(' at position " + current().getPosition());
+    }
+    private boolean isAtEnd() {
+        return current().getType() == TokenType.EOF;
+    }
+
+    private Token current() {
+        return tokens.get(position);
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) position++;
+        return tokens.get(position - 1);
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) {
+            return false;
+        }
+        return current().getType() == type;
+    }
+
+    private Token previous() {
+        return tokens.get(position - 1);
+    }
+}
